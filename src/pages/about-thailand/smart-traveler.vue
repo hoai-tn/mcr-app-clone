9c404b9727ae6821cc6f0d90e479b50eee238c01
hoai-tn <tranngochoai02051999@gmail.com>
2023-03-02 21:23:51 +0700

    #none page 39

---
+++ src/pages/about-thailand/smart-traveler.vue
@@ -0,0 +1,106 @@
+<template>
+  <v-container grid-list-xl>
+    <v-layout wrap>
+      <v-flex xs12 lg6>
+        <h1>Smart
+          Traveller
+          Advice</h1>
+        <p><strong>The following information is a summary
+            of the Smart Traveller Advice produced by
+            the Australian Governments Department
+            of Foreign Affairs and Trade (as of
+            January 2023)</strong></p>
+        <p>We advise you to exercise normal safety
+          precautions in Thailand. You should
+          exercise common sense and look out for
+          suspicious behaviour, as you would in
+          Australia and New Zealand. Monitor the
+          media and other sources for changes to
+          local travelling conditions.</p>
+        <p>Thailand regulates the import of
+          prescription and non-prescription
+          medication. Some medications require
+          a letter from the prescribing doctor.
+          Contact the Thailand High Commission
+          before you travel to check the rules
+          regarding any medicines you take. Keep
+          your medication in its original packaging.</p>
+        <p>Border areas near Cambodia, Myanmar
+          and Malaysia are dangerous due to
+          violence, armed conflict and landmines.
+          Pay close attention to your personal
+          security. Don’t travel to the 4 most
+          southern provinces of the Thailand-Malaysia border: Yala, Pattani, Narathiwat
+          and Songkhla. These areas experience
+          frequent violence and bombings.
+          Penalties for drug offences are severe
+          and include the mandatory death
+          sentence for drug trafficking.</p>
+        <p>Commercial surrogacy is illegal.
+          E-cigarettes, e-baraku, vaporisers and
+          refills are illegal. Smoking on some
+          beaches is illegal. Travel without carrying
+          identification is illegal. Thailand is a
+          multicultural but predominantly a
+          Buddhist country. There are conservative
+          standards of dress and behaviours in
+          many areas of Thailand.</p>
+        <p><strong>To register your trip on Smart Traveller, or
+            for more information visit:</strong><br><strong class="pink--text">smarttraveller.gov.au/Thailand </strong>
+        </p>
+      </v-flex>
+      <v-flex xs12 lg6>
+        <h1>Thailand
+          Do’s &
+          Don’ts</h1>
+        <h2>Thailand Do’s</h2>
+        <ul class="mb-3">
+          <li>Do respect Thai culture.</li>
+          <li>Do dress respectfully when visiting temples
+            and palaces. Knees and shoulders should
+            be covered.</li>
+          <li>Do respect all Buddha images. Buddha
+            images are sacred in Thailand and are not
+            to be climbed or sat on.</li>
+          <li>Do stand up when you hear the national
+            anthem or royal anthem.</li>
+          <li>Do take off your shoes before entering
+            temples and people’s homes.</li>
+          <li>Do return a wai (a traditional Thai greeting).</li>
+        </ul>
+        <h2>Thailand Don’ts</h2>
+        <ul>
+          <li>Don’t point to anything with your feet and
+            don’t place your feet on the table while
+            sitting. If you accidentally drop some
+            money, don’t step on it. This would be
+            insulting the King whose image is on Thai
+            currency.</li>
+          <li>Don’t raise your voice. In Thai culture,
+            keeping calm and not raising your voice are
+            qualities that are admired. Shouting and
+            getting angry won’t do you any favors.</li>
+          <li>Don’t touch monks. Women should never
+            touch a monk or hand anything directly to
+            them.</li>
+          <li>Don’t show disrespect to the Thai royal
+            family. Thailand has strict laws on this
+            which also cover social media.</li>
+          <li>Don’t take Buddha images out of the
+            country. Stores and market vendors will
+            probably still sell them to you without
+            telling you about the regulations.</li>
+          <li>Don’t touch a Thai person’s head. The head
+            is regarded as the most sacred part of the
+            body and shouldn’t be touched.</li>
+        </ul>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "smart-traveler",
+};
+</script>

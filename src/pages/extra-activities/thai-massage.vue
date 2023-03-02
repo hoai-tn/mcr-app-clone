27ab2d36207931e17804f166c17e422be9a25aff
hoai-tn <tranngochoai02051999@gmail.com>
2023-03-02 20:56:36 +0700

    #none page 36

---
+++ src/pages/extra-activities/thai-massage.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-container grid-list-xl>
+    <v-layout wrap>
+      <v-flex xs12>
+        <h1>Sukko Spa –
+          Thai Massage</h1>
+        <p>Sukko spa treatments are based on ancient Thai
+          medical theory. Traditional massages, heat therapies
+          and indigenous herbs are used to bring your physical
+          condition and spirit back to a state of balance.</p>
+        <p>All Sukko therapists are trained at the Sukko Spa
+          academy, certified by the ministry of education. To
+          ensure the highest standards all must complete a 500-
+          hour course before becoming part of the Sukko team.</p>
+        <p><strong>Aroma Body Massage</strong></p>
+        <p>This all-time popular type of massage is performed
+          with a healing touch accompanied by especially
+          blended aromatic oils. This is a holistic remedy. As
+          the massage is performed to benefit the vascular,
+          lymphatic, muscular and nervous systems of the body,
+          emotional health and well-being are also restored.</p>
+        <p>Adult: 60 minutes $3,580 THB<br>(Approximately $156 AUD / $170 NZD)</p>
+        <p><strong>Traditional Siam Body Massage</strong></p>
+        <p>Siam massage has a spiritual base, using theories of
+          energy flow and balance. Pressure is applied to key
+          points along the body’s energy channels to stimulate
+          proper energy flow. This helps to balance the senses
+          and works deeply to restore the body to normal
+          function for improved health.</p>
+        <p>Adult: 60 minutes $4,080 THB<br>(Approximately $177 AUD / $193 NZD)</p>
+        <h4>To book this tour, please visit an
+          EXO representative at the Tour desk,
+          located in the Hotel Lobby.</h4>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "thai-massage",
+};
+</script>

73d7e0e789ea4cfe45c9f3e3dbfb1fa6cf311d9d
hoai-tn <tranngochoai02051999@gmail.com>
2023-03-02 20:31:06 +0700

    #none page 29

---
+++ src/pages/extra-activities/art-phuket.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-container grid-list-xl>
+    <v-layout wrap>
+      <v-flex xs12>
+        <h1>It’s about
+          Art
+          Phuket</h1>
+        <p>You will be picked up at 08:00. Today, we take you deeper
+          into the art scene in Phuket (because there is defi nitely
+          more than the copy and paste art you fi nd in the tourist
+          areas like Patong). The day starts with a drive towards
+          the coast to visit Phuket town’s small streets with local
+          artists at their galleries & homes and feel free to ask the
+          artists themselves about their paintings (translation by
+          our guide). Stroll around streets with antique shops and
+          art galleries and have a look at what’s “art” in Phuket.
+          Finally, continue to visit local artists (paintings, batik
+          and sculptures – contemporary and classical) and have
+          a chance to follow a short workshop and create a small
+          piece of your own art.</p>
+        <p><strong>Time:</strong> 08.30 - 13.00</p>
+        <p><strong>Departure:</strong> Daily</p>
+        <p><strong>Note:</strong> Joint basis</p>
+        <p><strong></strong></p>
+        <p>Adult: $2,230 THB <br>(Approximately $97 AUD / $106 NZD)</p>
+        <p>Child: $1,950 THB (Child 2-11 Years)<br>(Approximately $85 AUD / $92 NZD)</p>
+        <h4>To book this tour, please visit an
+          EXO representative at the Tour desk,
+          located in the Hotel Lobby</h4>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "art-phuket",
+};
+</script>

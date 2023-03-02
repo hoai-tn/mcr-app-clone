e3d1c89cedf7fd51497483e1d1faac9088d81c89
hoai-tn <tranngochoai02051999@gmail.com>
2023-03-02 21:04:14 +0700

    #none page 37-2

---
+++ src/pages/extra-activities/hd-phuket.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-container grid-list-xl>
+    <v-layout wrap>
+      <v-flex xs12>
+        <h1>HD Phuket
+          E-Bike Coast
+          to Coast
+          Cycling Tour</h1>
+        <p>You will be picked up at 08:30 Transfer to meeting point in
+          Choengthale, Laguna for the tour briefing and bike fitting. After
+          the bike fitting, we start our fascinating ride on Phuket’s rural
+          roads tour will start 09:00.</p>
+        <p>An easy and relaxed E-Bike tour on the rural roads of Phuket
+          along Bangtao Beach and Laguna Golf course to the East Coast.
+          We will pass rubber trees, oil palm plantations, pineapple fields,
+          small villages and see a stunning view over Phang Nga Bay. We
+          will have a coffee break by the sea and stop for pictures at the
+          Laem Sai pier.</p>
+        <p>Adult: $3,360 THB<br>(Approximately $146 AUD / $160 NZD)</p>
+        <p>Same price for child</p>
+        <p><strong>Note:</strong> Minimum 6 persons</p>
+        <h4>To book this tour, please visit an
+          EXO representative at the Tour desk,
+          located in the Hotel Lobby.</h4>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "hd-phuket",
+};
+</script>

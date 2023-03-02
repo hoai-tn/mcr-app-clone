c3005fc4b33e4625f99968442eeaccad27fdda9a
hoai-tn <tranngochoai02051999@gmail.com>
2023-03-02 20:34:20 +0700

    #none page 30

---
+++ src/pages/extra-activities/phi-phi-island.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-container grid-list-xl>
+    <v-layout wrap>
+      <v-flex xs12>
+        <h1>Phi Phi
+          Island by
+          Speedboat</h1>
+        <p>You will be picked up at 08:00. Escape to a quieter, more natural side of southern Thailand with this
+          day trip from Phuket. Travel by speedboat across the
+          Andaman Sea to the six scenic Phi Phi Islands. The
+          journey to Phi Phi takes around 45 minutes, passing by
+          spectacular limestone karsts that jut out from the blue
+          seas. Sit back and relax, enjoying the views as the boat
+          cruises closer to paradise. Pass by Maya Bay, arguably one of Thailand’s most beautiful places, just for
+          sightseeing. The bay is so stunning, in fact, that it was
+          used as the setting for the famous Hollywood movie
+          ‘The Beach’.</p>
+        <p>Continue around the islands, Phi Phi Lay Cove, and
+          Monkey Beach. Each of these stretches of island boasts
+          swaying palm trees and white sands. There are also
+          calm, clear waters brimming with fi sh for those who
+          wish to discover Thailand’s underwater beauty.
+          During the day, lunch will be served at Tonsai Bay. This
+          southern stretch of Phi Phi Ley island offers sweeping
+          views of the surrounding sea. The afternoon is spent
+          cruising to Bamboo Islands and possibly other islands
+          nearby, for more chances to snorkel and swim in the
+          Andaman Sea.</p>
+        <p>After a spectacular day cruising the Phi Phi Islands,
+          it is time to return to Phuket. Transfer back by boat and
+          car, arriving at the hotel in the early evening. <strong>(L)</strong></p>
+        <p>* Note some parts of the Phi Phi islands are currently
+          closed to tourists by the Thai ministry</p>
+        <p><strong>Time:</strong> 08:00 - 17:30</p>
+        <p><strong>Departure:</strong> Daily</p>
+        <p><strong>Note:</strong> Join-in tour and transfers</p>
+        <p>Adult: $3,800 THB<br>(Approximately $165 AUD / $180 NZD)</p>
+        <p>Child: $3,200 THB (Child 2-11 Years)<br>(Approximately $139 AUD / $152 NZD) </p>
+        <h4>To book this tour, please visit an
+          EXO representative at the Tour desk,
+          located in the Hotel Lobby.</h4>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "phi-phi-island",
+};
+</script>

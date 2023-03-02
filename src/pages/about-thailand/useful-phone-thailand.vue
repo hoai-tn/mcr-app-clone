883921e02e3a3bcc6fc1141540089498abbf9c1b
hoai-tn <tranngochoai02051999@gmail.com>
2023-03-02 22:09:06 +0700

    #none page 44

---
+++ src/pages/about-thailand/useful-phone-thailand.vue
@@ -0,0 +1,160 @@
+<template>
+  <v-container grid-list-xl>
+    <v-layout wrap>
+      <v-flex xs12>
+        <h1>Useful Phone
+          Numbers - Thailand</h1>
+      </v-flex>
+      <v-flex xs12>
+        <v-layout wrap>
+          <v-flex xs12 lg6 class="pr-xl-14">
+            <v-layout wrap>
+              <v-flex xs12 lg6>
+                <div>
+                  <h2>GENERAL</h2>
+                  <p><strong>Police 191</strong><br><strong>Tourist Police 1155</strong><br><strong>Fire 199</strong></p>
+                </div>
+                <div>
+                  <h2>EMERGENCIES</h2>
+                  <p><strong>Medical Emergency National 1669</strong><br><strong>Ambulance National 1554</strong></p>
+                </div>
+                <div>
+                  <h2>HOSPITALS</h2>
+                  <p><strong>Bangkok Hospital Siriroj</strong></p>
+                  <p>44 Chalermprakiat Ror 9 Rd<br>Located between Big C and Tesco Lotus
+                    on the bypass road.
+                    <br>
+                    Tel: +66 (0)7 636 1888
+                    <br>
+                    Email: <a href="mailto:info@phuketinternationalhospital.com">info@phuketinternationalhospital.com</a>
+                    <br>
+                    Website: <a href="www.phuketinternationalhospital.com">www.phuketinternationalhospital.com</a>
+                  </p>
+                  <p><strong>Bangkok Phuket Hospital</strong></p>
+                  <p>2/1 Hongyok utis Rd, Phuket City<br>Located in Phuket City.
+                    <br>
+                    Tel: +66 (0)7 625 4421
+                    <br>
+                    Email: <a href="mailto:info@phukethospital.com">info@phukethospital.com</a>
+                    <br>
+                    Website: <a href="www.phukethospital.com">www.phukethospital.com</a>
+                  </p>
+                </div>
+              </v-flex>
+              <v-flex xs12 lg6>
+                <div>
+                  <p><strong>Mission Hospital Phuket</strong></p>
+                  <p>4/1 Thepkrasattri Rd<br>Amphur Muang, Phuket 83000
+                    <br>
+                    Tel: (66-76) 237220/6
+                    <br>
+                    Email: <a href="mailto:admin-pk@mission-hospital.org">admin-pk@mission-hospital.org</a>
+                    <br>
+                    Website: <a href=" www.missionhospitalphuket.com"> www.missionhospitalphuket.com</a>
+                  </p>
+                </div>
+                <div>
+                  <h2>Australian
+                    Consulate General
+                  </h2>
+                  <p>6th Floor CCM Complex<br>77/77 Chalermprakiat Rama 9 Road
+                    (Bypass road)
+                    <br>
+                    Muang Phuket 83000 THAILAND
+                    <br>
+                    Phone: +66 (0) 76 317 700
+                    <br>
+                    Fax: +66 (0) 76 317 743
+                    <br>
+                    Email: <a href="mailto: consular.phuket@dfat.gov.au"> consular.phuket@dfat.gov.au</a>
+                  </p>
+                </div>
+                <div>
+                  <h2>VAT Refunds</h2>
+                  <p>Goods purchased in Thailand are VAT
+                    inclusive. However, foreign visitors (with a
+                    few exceptions) have the benefit to receive
+                    a 7% VAT refund on luxury goods purchased
+                    from shops that participate in the ‘VAT Refund
+                    for Tourists’ scheme. When you see a ‘VAT
+                    Refund for Tourists’ sign, you can receive a
+                    7% refund of the VAT levied on goods at the
+                    shop. However, certain conditions apply, and
+                    you won’t be able to claim your refund until
+                    you depart Thailand from an international
+                    airport.</p>
+                </div>
+              </v-flex>
+            </v-layout>
+          </v-flex>
+          <v-flex xs12 lg6 class="pl-xl-14">
+            <v-layout wrap>
+              <v-flex xs12 lg6>
+                <div>
+                  <p><strong>How to claim a refund</strong></p>
+                  <p>Goods must be purchased from stores
+                    displaying the ‘VAT Refund For Tourists’ sign.
+                    VAT refunds only apply to goods taken out
+                    of Thailand within 60 days from the date of
+                    purchase.</p>
+                  <p>Goods must have a value of at least 2,000
+                    baht per day per store.</p>
+                  <p>On the day of purchase, present your
+                    passport and request the sales assistant
+                    at the store to complete the VAT Refund
+                    Application for Tourists form (P.P.10) and
+                    attach the original tax invoice to the form.
+                    Each application form must show a value of
+                    goods of 2,000 baht or more.</p>
+                  <p>Before departure, present your goods and
+                    VAT Refund Application for Tourists form to
+                    Customs officers for inspection before check-in. Luxury goods, such as jewellery, gold
+                    ornaments, watches, glasses, pens, mobile or
+                    smart phone, laptop and tablet, handbag, belt
+                    of which the value is 10000 baht or more per
+                    item and carry on goods which the value is
+                    50000 baht or more per item. The tourist is
+                    required to hand carry and show the goods
+                    again at the Vat refund for tourist office, after
+                    passing the immigration checkpoint.</p>
+                  <p>For purchases not exceeding 30,000 baht,
+                    refunds will be made in cash, i.e., Thai baht,
+                    by bank draft or credit to your credit card
+                    account. For purchases exceeding 30,000
+                    baht, the refund will be made by bank draft or
+                    credit to your credit card account.</p>
+                </div>
+              </v-flex>
+              <v-flex xs12 lg6>
+                <div>
+                  <h2>Tourist Privilege
+                    Cards – Central
+                    Department Stores</h2>
+                  <p>Central is the largest retailer in Thailand with
+                    a large store and lifestyle mall in Phuket.</p>
+                  <p>Simply flash your passport or ID card at the
+                    Information/Customer Service kiosks at any
+                    Central Department Store and you will be
+                    given a 5% Visitor Discount Card. You can
+                    also use this card at subsidiaries such as
+                    Robinsons and Super sports (not applicable
+                    at Tops Supermarket, Power buy, MGA or
+                    Marks & Spencer, the liquor store, pharmacy,
+                    fabrics, electrical appliances, gift exchange
+                    products, rental shops, or sales items). This
+                    card is valid for one week.</p>
+                </div>
+              </v-flex>
+            </v-layout>
+          </v-flex>
+        </v-layout>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "useful-phone-thailand",
+};
+</script>

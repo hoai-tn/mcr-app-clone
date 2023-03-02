fd1c2f21605717c3efb1c008516c82878ef08766
hoai-tn <tranngochoai02051999@gmail.com>
2023-03-02 20:22:56 +0700

    #none page 27

---
+++ src/pages/extra-activities/morning-elephants.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-container grid-list-xl text-center>
+    <v-layout wrap>
+      <v-flex xs12>
+        <h1 class="text-center">ExtraActivities</h1>
+        <h2>Meet and Greet Phuket’s gentle Giants</h2>
+        <h4>Visit the Phuket Elephant Sanctuary Learn about elephant protection. Enjoy responsible interaction with
+          elephants.</h4>
+        <p>Visit one of Thailand’s most acclaimed elephant sanctuaries and have distinct pleasure of learning about and
+          observing Asian elephants. This responsibly minded sanctuary allows visitors to experience
+          the majestic wonders of these gentle giants while ensuring their conservation and responsible treatment. The
+          perfect elephant experience in Thailand.</p>
+        <h4>PROGRAM DETAIL</h4>
+        <p>Be picked up at either 08:00 or 13:00. Enjoy a
+          magical morning or afternoon mingling with
+          rehabilitated Asian elephants at Phuket Elephant
+          Sanctuary. This scenic slice of paradise is sanctuary
+          and home for both elephants and forest wildlife. The
+          visit begins with a pleasant ride down a village lane
+          into the grounds of our sanctuary to the Tree Top
+          Reception and Observation Platform.</p>
+        <p>Have an opportunity to meet some of the elephants
+          and learn both who they are and why they need
+          protection. Then there’s a unique opportunity to
+          feed the elephants and join them on a leisurely walk
+          through the forest. Along the way, have the privilege
+          of observing their natural behaviour and watching as
+          they go about their daily activities such as foraging,
+          bathing, and playing in one of the three lagoons.</p>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "morning-elephants",
+};
+</script>

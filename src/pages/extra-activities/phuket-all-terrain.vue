a69a560d466d7ac76cd50a60f67106bb17faffcc
hoai-tn <tranngochoai02051999@gmail.com>
2023-03-02 20:51:41 +0700

    #none page 35

---
+++ src/pages/extra-activities/phuket-all-terrain.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-container grid-list-xl>
+    <v-layout wrap>
+      <v-flex xs12>
+        <h1>Phuket
+          All Terrain
+          (ATV)</h1>
+        <p>Upon being picked up from your hotel in Phuket,
+          you will be taken to discover Phuket by ATV,
+          follow your guide and explore the countryside
+          along natural tracks.</p>
+        <p>Encompassing all the scenery of the tropical
+          forest, you will see many different types of fl ora
+          & fauna and be able to observe the lifestyles of
+          the local people. This unique and adrenaline
+          pumping tour allows you to explore the interior
+          of Phuket without the crowds.</p>
+        <p><strong>Time:</strong> 08.00 – 12.00 / 14.00 – 18.00</p>
+        <p><strong>Departure:</strong> Daily</p>
+        <p><strong>Note: </strong> Joint basis</p>
+        <p>Adult: $3,100 THB (Driver)<br>(Approximately $135 AUD / $147 NZD)</p>
+        <p>$2,300 THB (Passenger)<br>(Approximately $100 AUD / $109 NZD)</p>
+        <p>Child: $1,950 THB (Passenger)<br>(Approximately $85 AUD / $92 NZD)</p>
+        <h4>To book this tour, please visit an
+          EXO representative at the Tour desk,
+          located in the Hotel Lobby.</h4>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "phuket-all-terrain",
+};
+</script>

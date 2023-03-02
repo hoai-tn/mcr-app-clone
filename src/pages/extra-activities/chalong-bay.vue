8ee58e8ba28f48df9928264dd845cecb13d73680
hoai-tn <tranngochoai02051999@gmail.com>
2023-03-02 20:45:45 +0700

    #none page 33

---
+++ src/pages/extra-activities/chalong-bay.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-container grid-list-xl>
+    <v-layout wrap>
+      <v-flex xs12>
+        <h1>Chalong
+          Bay Rum
+          Experience</h1>
+        <p>Visit the distillery where Chalong Bay Rum,
+          (Gold Medallist in the San Francisco World Spirit
+          Championships) is produced. An exclusive to
+          Phuket!</p>
+        <p>This entertaining tour includes; An overview
+          of spirits and their definition, history of sugar
+          cane growing and its native region, information
+          about industrial and natural rum production and
+          details of the step-by-step process throughout
+          the distillery.</p>
+        <p>This is followed by a blind tasting competition
+          with a rum expert, sampling 3-types of rum
+          where you need to guess the flavours from a list.
+          The winner receives a prize.</p>
+        <p><strong>Time:</strong> 10.00 - 13.00 / 13.00 – 16.00</p>
+        <p><strong>Departure:</strong> Daily</p>
+        <p><strong>Note:</strong> Joint basis</p>
+        <p>Adult: $3,980 THB<br>(Approximately $173 AUD / $189 NZD)</p>
+        <h4>To book this tour, please visit an
+          EXO representative at the Tour desk,
+          located in the Hotel Lobby.</h4>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "chalong-bay",
+};
+</script>

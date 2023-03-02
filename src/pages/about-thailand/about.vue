dcb6341b1857ef41dc42080fd3a4c6fe4ecc14c5
hoai-tn <tranngochoai02051999@gmail.com>
2023-03-02 21:13:30 +0700

    #none page 38

---
+++ src/pages/about-thailand/about.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-container grid-list-xl>
+    <v-layout wrap>
+      <v-flex xs12>
+        <h1>About
+          Thailand</h1>
+        <p>Called the “Land of Smiles,”
+          Thailand has become one of the
+          leading travel destinations in Asia
+          over the last few decades. Officially
+          known as the Kingdom of Thailand,
+          this Southeast Asian nation is the
+          only country in the region to have
+          avoided colonialism, remaining a
+          sovereign country in its own right.
+          Economically, Thailand is still
+          developing, although this doesn’t
+          stop it from dominating much of the
+          region’s rice and tourism industries.</p>
+        <p>Home to nearly 11 million people, the
+          Thailand’s capital, Bangkok is a vast
+          network of busy streets, commercial
+          centres and modern delights that hug
+          the meandering banks of the Chao
+          Phraya River. The perfect balance of
+          city, country, mountains and water,
+          Thailand is a dream for both nature
+          lovers and urban dwellers with cheap
+          massages on every corner.</p>
+        <p class="text-h3 pink--text">A land of Smiles</p>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "about",
+};
+</script>

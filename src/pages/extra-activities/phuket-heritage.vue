15b370c0693cc9deadd114c4121c142fe3056caf
hoai-tn <tranngochoai02051999@gmail.com>
2023-03-02 20:48:56 +0700

    #none page 34

---
+++ src/pages/extra-activities/phuket-heritage.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-container grid-list-xl>
+    <v-layout wrap>
+      <v-flex xs12>
+        <h1>Phuket
+          Heritage</h1>
+        <p>Uncover the rich and colourful cultural heritage
+          of Phuket and its original Peranakan settlers
+          as you wander through the historic quarters
+          of this old provincial island town with a local
+          guide. The fascinating array of exotic tropical
+          fruits available in the downtown fruit market will
+          tantalize and satisfy your palate.</p>
+        <p>Explore the oldest Chinese shrine and
+          uniqueness of the Sino-Portuguese architecture
+          that are standing testimonies to the influence of
+          the Chinese migrants during the tin mining days
+          of the 19th century.</p>
+        <p>Also visit one of Phuket’s most important
+          temples and take in the sights at a scenic point
+          with sweeping views of the island.</p>
+        <p><strong>Time:</strong> 09.00 - 13.00 / 13.00 – 17.00</p>
+        <p><strong>Departure:</strong> Daily</p>
+        <p><strong>Note:</strong> Joint basis</p>
+        <p>Adult: $3,060 THB<br>(Approximately $133 AUD / $145 NZD)</p>
+        <p>Child: $2,600 THB (Child 2-11 Years)<br>(Approximately $113 AUD / $123 NZD)</p>
+        <p><strong>Note:</strong> Minimum 6 persons</p>
+        <h4>To book this tour, please visit an
+          EXO representative at the Tour desk,
+          located in the Hotel Lobby.</h4>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "phuket-heritage",
+};
+</script>

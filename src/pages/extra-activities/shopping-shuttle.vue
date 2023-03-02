30b269b5688df9643047a047419787fadfaa7568
hoai-tn <tranngochoai02051999@gmail.com>
2023-03-02 21:00:43 +0700

    #none page 37-1

---
+++ src/pages/extra-activities/shopping-shuttle.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-container grid-list-xl>
+    <v-layout wrap>
+      <v-flex xs12>
+        <h1>Shopping
+          Shuttle</h1>
+        <p>Enjoy all that Phuket has to offer on
+          the retail front, visiting major shopping
+          centres at Central Festival, Jungceylon
+          and Central Phuket.</p>
+        <p>Central plays host to over 120 outlets
+          selling products ranging from silk to
+          sushi, from Levi’s to laptops, from
+          books to barbecues - if it isn’t on sale
+          at Central you probably didn’t need it
+          anyway! It’s literally possible to spend
+          a full day here - shopping, catching
+          the latest movies, and dining out at the
+          range of international restaurants that
+          offer everything from French cuisine to
+          Japanese to local Thai outlets.</p>
+        <p>At Jungceylon the 200-plus stores in
+          this shopping heaven sell brand-name
+          clothes, specs, sunglasses, cosmetics
+          and perfumes, shoes, DVDs, computer
+          games, tons of mobile phones and
+          accessories, electrical goods but it is
+          also a great entertainment haven with
+          a five-room SF Cinema and a 16-lane
+          bowling alley, perfect for rainy days or
+          to escape the heat of the day.</p>
+        <p><strong>8-seat van shuttles leave the
+            hotel at:</strong><br>10.30, 13.30, 16.30 and return at 13.30,
+          16.30, 19.30</p>
+        <p>Roundtrip price: THB $400 per person <br>
+          (Approximately $17 AUD / $19 NZD)</p>
+        <p><strong>Note:</strong> Minimum 6 persons</p>
+        <h4>To book this tour, please visit an
+          EXO representative at the Tour desk,
+          located in the Hotel Lobby.</h4>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "shopping-shuttle",
+};
+</script>

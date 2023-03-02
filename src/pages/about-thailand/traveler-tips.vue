c907a10015ce94e257972e074c248740c68a650a
hoai-tn <tranngochoai02051999@gmail.com>
2023-03-02 21:45:41 +0700

    #none page 40 41

---
+++ src/pages/about-thailand/traveler-tips.vue
@@ -0,0 +1,315 @@
+<template>
+  <v-container grid-list-xl>
+    <v-layout wrap>
+      <v-flex xs12>
+        <h1>Traveller Tips
+          for Thailand</h1>
+      </v-flex>
+      <v-flex xs12>
+        <v-layout wrap>
+          <v-flex xs12 lg6>
+            <v-layout wrap>
+              <v-flex xs12 lg6>
+                <div>
+                  <h2>THE PEOPLE & LANGUAGE</h2>
+                  <p> Over 95 percent of Thailand’s
+                    population is Buddhist, with Muslim,
+                    Christian, Hindu and animist
+                    minorities. The national language is
+                    Thai, though English is widely spoken
+                    in Bangkok and most major tourist
+                    centres.</p>
+                  <p>The King and Thai monarchy are
+                    widely revered and respected in
+                    Thailand and Lese Majeste laws are
+                    in force. It is a criminal offence to
+                    defame or criticize the monarchy in
+                    any way either in conversation or in
+                    writing. </p>
+                </div>
+                <div>
+                  <h2>VISAS</h2>
+                  <p>Most nationalities qualify for a
+                    30-day visa on arrival. Those who
+                    do not qualify can usually obtain a
+                    15-day visa on arrival (Two photos
+                    needed) Some exceptions do however
+                    apply, please contact your closest
+                    Thai embassy or consulate for more
+                    information on visa requirements.</p>
+                </div>
+                <div>
+                  <h2>CUSTOMS</h2>
+                  <p>Duty free allowance for visitors is
+                    200 cigarettes and one litre of wine
+                    or spirits. Importing weapons and
+                    pornography is prohibited. Penalties
+                    for attempting to smuggle drugs are
+                    very severe in Thailand, including life
+                    imprisonment.</p>
+                  <p>Any amount of traveller’s cheques
+                    or drafts may be imported, but cash
+                    in excess of USD 10,000 must be
+                    declared. Not more than THB 50,000
+                    in cash may be exported. Antiques
+                    and Buddha images require export
+                    licenses.</p>
+                </div>
+              </v-flex>
+              <v-flex xs12 lg6>
+                <div>
+                  <h2>ARRIVAL FORMALITIES</h2>
+                  <p>Passengers who will transit in
+                    Bangkok before an onward domestic
+                    flight must clear immigration on
+                    arrival in Bangkok before proceeding
+                    to the transit gate for the onward
+                    flight. In most cases your luggage will
+                    be through-checked from the point of
+                    origin to the final destination. </p>
+                </div>
+                <div>
+                  <h2>CLOTHING</h2>
+                  <p>Comfortable lightweight clothing
+                    in natural fabrics such as cotton is
+                    suitable for traveling in Thailand. It
+                    is advisable to cover arms and legs
+                    in the evenings and remember to
+                    use repellent against biting insects
+                    at most times. A lightweight raincoat
+                    and umbrella are a good idea for any
+                    occasional showers and an umbrella
+                    can also offer useful shade from
+                    the sun.</p>
+                  <p>Visitors should not wear shorts, short
+                    skirts or other revealing clothing
+                    and shoulders should be covered
+                    when visiting temples, monasteries
+                    or venues related to the monarchy.
+                    Shoes must be removed before
+                    entering religious buildings or a
+                    private home. It is therefore useful to
+                    wear shoes without too many laces
+                    and which can easily be taken off.</p>
+                </div>
+              </v-flex>
+            </v-layout>
+          </v-flex>
+          <v-flex xs12 lg6>
+            <v-layout wrap>
+              <v-flex xs12 lg6>
+                <div>
+                  <h2>HEALTH</h2>
+                  <p>Certificates of inoculation for
+                    cholera or yellow fever are required
+                    if you arrive from an infected area.
+                    Some border areas of Thailand are
+                    malarial, and visitors should take the
+                    necessary precautions. Please check
+                    with your local health practitioner for
+                    the latest advice.</p>
+                </div>
+                <div>
+                  <h2>CURRENCY</h2>
+                  <p>Thai currency is the baht, divided
+                    into 100 satang. Notes come in
+                    B1,000, B500, B100, B50 and B20
+                    denominations. There are also B10,
+                    B5, B2, B1, 50 satang and 25 satang
+                    coins. Cash and travellers’ cheques
+                    can be readily exchanged with banks
+                    and moneychangers. Major credit
+                    cards are widely accepted in hotels,
+                    restaurants, and shops. Give a 10%
+                    tip at restaurants if a service charge
+                    is not included. Tip porters and bag
+                    boys B20. There’s no need to tip taxi
+                    drivers.</p>
+                </div>
+                <div>
+                  <h2>LOCAL TIME</h2>
+                  <p>Thailand time is GMT +7 hours.</p>
+                </div>
+                <div>
+                  <h2>ELECTRICITY</h2>
+                  <p>Electric current is usually 220 volts
+                    A/C (50 cycles). Various plugs and
+                    sockets are used. Most hotels supply
+                    adapters and transformers for 110V
+                    equipment.</p>
+                </div>
+              </v-flex>
+              <v-flex xs12 lg6>
+                <div>
+                  <h2>BUSINESS HOURS</h2>
+                  <p>Government offices are open Monday
+                    to Friday from 08:30am to 16:30 pm
+                    except on public holidays. Business
+                    offices generally operate between
+                    09:00 am and 17:00 pm with some
+                    opening on Saturday mornings. Banks
+                    open at 08:30 am Monday to Friday
+                    and close at 15:30 pm. Shopping
+                    centres usually open at 10.00 until
+                    22.00.</p>
+                </div>
+                <div>
+                  <h2>SHOPPING</h2>
+                  <p>Thailand offers some good shopping
+                    and many day and nights bazaars.
+                    Only buy jewellery at reputable
+                    shops and insist on a certificate
+                    guaranteeing the quality. Avoid street
+                    touts.</p>
+                </div>
+                <div>
+                  <h2>FOOD</h2>
+                  <p>Food is usually prepared and served
+                    with the ubiquitous fish sauce called
+                    nam pla. Typical Thai dishes are
+                    served either with rice or noodles and
+                    a meal usually constitutes a variety
+                    of dishes (Meat, vegetables, fish or
+                    eggs) served family style for sharing.
+                    Due to the strong Buddhist influence
+                    in Thailand, vegetarian food is widely
+                    available. If you are allergic to fish
+                    or fish derivatives, seafood or nuts
+                    please be sure to advise the venue as
+                    these ingredients are widely used in
+                    many Thai dishes.</p>
+                </div>
+              </v-flex>
+            </v-layout>
+          </v-flex>
+          <v-flex xs12 lg6>
+            <v-layout wrap>
+              <v-flex xs12 lg6>
+                <div>
+                  <h2>WATER</h2>
+                  <p>It is not advisable to drink tap water
+                    but bottled mineral water is safe
+                    and available everywhere. All hotels
+                    provide a complimentary bottle of
+                    local mineral water per person in
+                    the room. Ice cubes in drinks are
+                    usually made from purified water
+                    and are usually not a problem. Some
+                    minor stomach problems are always
+                    possible when travelling in exotic
+                    countries. Bring a supply of your
+                    usual anti-diarrhea medicine.</p>
+                </div>
+                <div>
+                  <h2>SAFETY</h2>
+                  <p>On the whole Thailand is a safe
+                    country to visit and its people are
+                    renowned for their warm friendliness
+                    and hospitality. As a global rule,
+                    however, never leave your belongings
+                    unattended and always maintain eye
+                    contact or a firm grip on cameras and
+                    shoulder bags. Taxis are relatively
+                    cheap and easily accessed. Ask the
+                    hotel or restaurant to call one for
+                    you or simply hail a cab in the street.
+                    Always ask to turn on the meter (In
+                    Thai language this sounds like ‘Bert
+                    meter kab’</p>
+                </div>
+              </v-flex>
+              <v-flex xs12 lg6>
+                <div>
+                  <h2>INTERNET</h2>
+                  <p>Broadband Wi-Fi internet is
+                    commonplace in Thailand and
+                    speeds are very good. Most hotels,
+                    restaurants and coffee shops offer
+                    complimentary access these days. </p>
+                </div>
+                <div>
+                  <h2>TELEPHONE</h2>
+                  <p>If you have worldwide coverage, you
+                    can bring your own mobile phone
+                    and use it to make domestic or
+                    international calls. Check with your
+                    mobile phone provider for the costs
+                    before using it abroad - it may be
+                    expensive.</p>
+                  <p>Most hotels now have IDD phones in
+                    rooms. It may not always be possible
+                    to make international calls in remote
+                    areas.</p>
+                </div>
+              </v-flex>
+            </v-layout>
+          </v-flex>
+          <v-flex xs12 lg6>
+            <v-layout wrap>
+              <v-flex xs12>
+                <h2>GETTING AROUND PHUKET</h2>
+                <v-img src="../../assets/about-thailand/shuttle-bus-schedule.png"></v-img>
+              </v-flex>
+              <v-flex xs12 lg6>
+                <div>
+                  <h2>Tuk-Tuks</h2>
+                  <p>For short distances tuk tuks are
+                    fine, but their small size makes
+                    them uncomfortable for anything
+                    longer than about 30 minutes. In the
+                    main resorts like Patong, even short
+                    distances cost around 200 Baht. You
+                    should state your destination and
+                    agree on the price before setting
+                    off! Trips further afield, beyond the
+                    immediate beach
+                    area can cost anything up to 400 Baht
+                    - depending on the distance and your
+                    bargaining skills!</p>
+                </div>
+                <div>
+                  <h2>Taxis & Grab Taxi App</h2>
+                  <p>Metred taxis are available for hire
+                    in most areas – The hotel concierge
+                    will be able to call one for you. Fares
+                    are set at 50 Baht for the first two
+                    kilometres and 7 Baht per additional
+                    kilometre, though for longer distances
+                    flat rates may be charged. Airconditioned car taxis can be hired at
+                    all the main resort areas with rates
+                    similar to the tuk tuks. Expect to
+                    bargain your fare in advance.</p>
+                </div>
+              </v-flex>
+              <v-flex xs12 lg6>
+                <div>
+                  <h2>Local Buses (Songthaews)</h2>
+                  <p>These local open-air buses (painted
+                    bright blue for easy recognition)
+                    run regularly between 7.00am and
+                    6.00pm and have the destination
+                    written in English on the outside of
+                    the bus. There are no designated
+                    bus stops, which means you can
+                    stop them anywhere on the route. In
+                    Patong, your best spot for catching a
+                    bus, is along the beach road outside
+                    the tourist police box. Standard cost
+                    for trips to/from Patong to locations
+                    such as Phuket Town, Surin and
+                    Kamala is around 40 Baht per person.</p>
+                </div>
+              </v-flex>
+            </v-layout>
+          </v-flex>
+        </v-layout>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "traveler-tips",
+};
+</script>

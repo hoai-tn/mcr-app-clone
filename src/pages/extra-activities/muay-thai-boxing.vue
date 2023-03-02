81f9a078e97a69fd934f935f8c70306b29d17d36
hoai-tn <tranngochoai02051999@gmail.com>
2023-03-02 20:42:54 +0700

    #none page 32

---
+++ src/pages/extra-activities/muay-thai-boxing.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-container grid-list-xl>
+    <v-layout wrap>
+      <v-flex xs12>
+        <h1>Muay Thai
+          Boxing Camp</h1>
+        <p>At this Muay Thai workshop, experience the art of
+          straight forward martial arts. Discover how to use
+          your hands, elbows, and knees to execute great
+          kicks and punches. This versatile sport is not
+          just a good form of defence but also an integral
+          part of Thai culture and tradition.</p>
+        <p>Start off with some expert coaching and insights
+          into the rituals and music performed during
+          a fight, including the Wai Kru where fighters
+          pay homage to their trainers. Then the experts
+          will coach you in the various moves and
+          manoeuvres of the art before you have a go
+          yourself!</p>
+        <p>Casual sportswear/shorts and T-shirts required.</p>
+        <p><strong>Time:</strong> 08.30 - 13.00 / 13.00 – 17.00</p>
+        <p><strong>Departure:</strong> Daily</p>
+        <p><strong>Note:</strong> Joint basis</p>
+        <p>Adult: THB $2,450<br>(Approximately $107 AUD / $116 NZD)</p>
+        <p>Child: THB $2,050 (Child 2-11 Years)<br>(Approximately $89 AUD / $97 NZD)</p>
+        <h4>To book this tour, please visit an
+          EXO representative at the Tour desk,
+          located in the Hotel Lobby.</h4>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "muay-thai-boxing",
+};
+</script>

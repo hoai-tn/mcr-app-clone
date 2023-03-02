e1d7caa2daf035cc98645421e34158406b004654
hoai-tn <tranngochoai02051999@gmail.com>
2023-03-02 20:37:48 +0700

    #none page 31

---
+++ src/pages/extra-activities/simillan-island.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-container grid-list-xl>
+    <v-layout wrap>
+      <v-flex xs12>
+        <h1>Similan Island
+          Snorkelling by
+          Speedboat</h1>
+        <p>You will be picked up at 07:00. The Similan
+          Islands are protected and a marine national
+          park of 12,000 hectares, made of 9 islands in
+          the middle of the Andaman Sea. The islands are
+          all made of granite, giving them a rare natural
+          beauty with unique formations. The Similan
+          Islands are famous for their 30 stunning and
+          rare diving spots with walls of corals, myriad of
+          sponges, rocks, thousands of fi sh species, and
+          stunning colours of the seas and its flora.</p>
+        <p>The Similan Islands have a very rich
+          environment like no other place in the world. On
+          the Similan Islands, you will fi nd many beautiful
+          spots in shallow waters, which can easily be
+          discovered with a simple mask and snorkel. For
+          those sea lovers, this is the spot for you, and you
+          will keep an amazing souvenir of the Similan
+          Islands. <strong>(L)</strong></p>
+        <p><strong>Time:</strong> 07.00 – 17.00</p>
+        <p><strong>Departure:</strong> Daily</p>
+        <p><strong>Note:</strong> Join-in tour and transfers</p>
+        <p>Adult: $3,700 THB<br>(Approximately $161 AUD / $175 NZD)</p>
+        <p>Child: $3,210 THB (Child 2-11 Years)<br>(Approximately $139 AUD / $152 NZD)</p>
+        <h4>To book this tour, please visit an
+          EXO representative at the Tour desk,
+          located in the Hotel Lobby.</h4>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "simillan-island",
+};
+</script>
